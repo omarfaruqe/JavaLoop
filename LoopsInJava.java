f67d9a1d65caecd5aef92ef02e051f4391ef6104
Omar Faruqe <faruqe.cse@gmail.com>
2021-03-24 22:38:51 +0600

    Yahtzee with 5 dices

--- LoopsInJava.java
+++ LoopsInJava.java
@@ -62,14 +62,35 @@ public class LoopsInJava{
 	public int keepRooling(){
 		int dice1 = rollDice();
 		int dice2 = rollDice();
+		int dice3 = rollDice();		
 		int count = 1;
-		while(!(dice1 == dice2)){
+		while(!(dice1 == dice2 && dice2 == dice3)){  // dice1 == dice2 == dice3 will show compiler error
 			// we need to re-roll
 			int dice1 = rollDice();
-			int dice2 = rollDice();		
+			int dice2 = rollDice();	
+			int dice3 = rollDice();	
 			count = count + 1;	
 		}
 		return count;
+	}
+
+	public int keepRolling() {
+	    int dice1 = rollDice();
+	    int dice2 = rollDice();
+	    int dice3 = rollDice();
+	    int dice4 = rollDice();
+	    int dice5 = rollDice();    
+	    int count = 1;
+	    while (!(dice1 == dice2 && dice2 == dice3 && dice3 == dice4 && dice4 == dice5)) {
+	        //we need to re-roll
+	        dice1 = rollDice();
+	        dice2 = rollDice();
+	        dice3 = rollDice();
+	        dice4 = rollDice();
+	        dice5 = rollDice();         
+	        count = count + 1;
+	    }
+	    return count;
 	}
 
 
