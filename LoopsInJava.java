465f01a0fc09c28e1a4b017fbccaf8bcd789afed
Omar Faruqe <faruqe.cse@gmail.com>
2021-03-24 22:30:27 +0600

    Yahtzee with two dice only

--- LoopsInJava.java
+++ LoopsInJava.java
@@ -50,4 +50,27 @@ public class LoopsInJava{
 		System.out.println("Now it's not raining anymore");		
 	}
 
+
+	// Yahtzee (roll 5 dice and all showup with same face)
+	public int rollDice(){
+		double randomNumber = Math.random();
+		randomNumber = randomNumber * 6;
+		randomNumber = randomNumber + 1;
+		int randomInt = (int) randomNumber;
+		return randomInt;
+	}
+	public int keepRooling(){
+		int dice1 = rollDice();
+		int dice2 = rollDice();
+		int count = 1;
+		while(!(dice1 == dice2)){
+			// we need to re-roll
+			int dice1 = rollDice();
+			int dice2 = rollDice();		
+			count = count + 1;	
+		}
+		return count;
+	}
+
+
 }
