20d2f57f9670dfb0a0dfd7f0e465ae3f3ae638c8
Omar Faruqe <faruqe.cse@gmail.com>
2021-03-24 22:10:47 +0600

    Calling checkAlarm() multiple times

--- LoopsInJava.java
+++ LoopsInJava.java
@@ -4,20 +4,33 @@ public class LoopsInJava{
 	* makes a single beep sound
 	*/	
 	public void beep(){
-
+		// makes a single beep sound
 	}
 
 	/**
 	* @return true if alarm is on, false if off
 	*/	
 	public boolean checkAlarm(){
-
+		return true;
 	}
 	
 	/**
 	* keep beeping untool snoozed
 	*/
 	public void alarm(){
-
+		boolean on = checkAlarm();
+		if(on){
+			beep();
+			on = checkAlarm();
+		}
+		if(on){
+			beep();
+			on = checkAlarm();
+		}
+		if(on){
+			beep();
+			on = checkAlarm();
+		}
+		// ....		
 	}
 }
