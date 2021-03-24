c56f7d59fc6632f4c2a14dd7a3744f4c4fcabdbe
Omar Faruqe <faruqe.cse@gmail.com>
2021-03-24 22:07:21 +0600

    Basic alarm functions

---
+++ LoopsInJava.java
@@ -0,0 +1,23 @@
+public class LoopsInJava{
+
+	/**
+	* makes a single beep sound
+	*/	
+	public void beep(){
+
+	}
+
+	/**
+	* @return true if alarm is on, false if off
+	*/	
+	public boolean checkAlarm(){
+
+	}
+	
+	/**
+	* keep beeping untool snoozed
+	*/
+	public void alarm(){
+
+	}
+}
