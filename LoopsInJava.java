13bb807f9e642dfd3ceeda087e2f3accda53698b
Omar Faruqe <faruqe.cse@gmail.com>
2021-03-27 23:00:55 +0600

    TODO: Finding longest name in an array

--- LoopsInJava.java
+++ LoopsInJava.java
@@ -223,9 +223,9 @@ public class LoopsInJava{
 	}
 
 	/**
-	 * 
-	 * @param temparatures
-	 * @return
+	 * Calculate the average temperature
+	 * @param temparatures array of temperature
+	 * @return double average value of temperature
 	 */
 	public double calculateAverate(double temparatures[]){
 		int size = temparatures.length;
@@ -253,4 +253,22 @@ public class LoopsInJava{
 		}
 		return min;
 	}
+	
+	/*
+	You should implement the function String findLongestName(String [] names) 
+	which takes an array of Strings as an input containing a list of names, 
+	and return the String that has the longest name.
+
+	To do so, try to follow these steps:
+	The first step is to calculate and store the length of the input array, 
+	this is done using names.length; and store that in an integer variable.
+
+	Then create a new String called longestName that will store the longest name in the array of names, 
+	initialize it with the first name in the array, that is names[0].
+
+	Next, you should create a for loop that will compare every name in the array using names[i] against the longestName. 
+	Only replace the longestName value if the names[i] is longer .
+
+	Finally, return the longestName variable as the return value of the function 
+	*/
 }
